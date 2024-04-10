d6393fddec55d5143037b165e06adfac1f2f7d9a
Justin Zhao <justin@justinzhao.net>
2024-04-09 23:09:23 -0400

    created docker compose

--- machine-learning-client/main.py
+++ machine-learning-client/main.py
@@ -7,21 +7,32 @@ GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
 GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
+import os
+
+# get the current working directory
+current_working_directory = os.getcwd()
+print(current_working_directory)
+
 video = cv2.VideoCapture(0)
 
 # Create a image segmenter instance with the live stream mode:
 def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
+    # cv2.imshow('Show', output_image.numpy_view())
+    # imright = output_image.numpy_view()
     print(result.gestures)
+    # cv2.imwrite('somefile.jpg', imright)
+
 
 options = GestureRecognizerOptions(
-    base_options=BaseOptions(model_asset_path='gesture_recognizer.task'),
+    base_options=BaseOptions(model_asset_path=current_working_directory+'\gesture_recognizer.task'),
     running_mode=VisionRunningMode.LIVE_STREAM,
     result_callback=print_result)
 
-
 timestamp = 0
 with GestureRecognizer.create_from_options(options) as recognizer:
+  # The recognizer is initialized. Use it here.
     while video.isOpened(): 
+        # Capture frame-by-frame
         ret, frame = video.read()
 
         if not ret:
@@ -30,6 +41,10 @@ with GestureRecognizer.create_from_options(options) as recognizer:
 
         timestamp += 1
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+        # Send live image data to perform gesture recognition
+        # The results are accessible via the `result_callback` provided in
+        # the `GestureRecognizerOptions` object.
+        # The gesture recognizer must be created with the live stream mode.
         recognizer.recognize_async(mp_image, timestamp)
 
         if cv2.waitKey(5) & 0xFF == 27:
