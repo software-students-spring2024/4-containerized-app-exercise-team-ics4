b0549d281cb5a100b8bfdf9e6be36c7022c8698e
Justin Zhao <justin@justinzhao.net>
2024-04-16 00:22:02 -0400

    updated linted code

--- machine-learning-client/tests/conftest.py
+++ machine-learning-client/tests/conftest.py
@@ -1,36 +1,19 @@
 import pytest
-from flask import Flask
-from mongomock import MongoClient
 from unittest.mock import patch
-
+import mongomock
 from app import app as flask_app
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope='session', autouse=True)
+def mock_db():
+    with patch('pymongo.MongoClient', new_callable=lambda: mongomock.MongoClient):
+        yield
+
+@pytest.fixture(scope='session')
 def app():
-    # use mongo mock to isolate testing from prod db
-    with patch('pymongo.MongoClient', MongoClient):
-        db = MongoClient().audio_feed
-        collection = db.status
-        status_object = {
-            "loading": True,
-            "sound1": "",
-            "confidence1": "",
-            "sound2": "", 
-            "confidence2": "",
-            "sound3": "",
-            "confidence3": "",
-            "microphoneConnected": False
-        }
-        collection.delete_many({})
-        collection.insert_one(status_object)
-
-        flask_app.config.update({
-            "TESTING": True,
-            "MONGO_URI": "mongomock://localhost"
-        })
-
+    flask_app.config['TESTING'] = True
+    with flask_app.app_context():
         yield flask_app
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope='session')
 def client(app):
     return app.test_client()
